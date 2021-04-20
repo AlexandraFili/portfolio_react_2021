b8f686b85ad8b053a964cdd4b0a075326b0fe5fe
Sandra <Alexandra.Filippou@student.tafesa.edu.au>
2021-04-20 16:05:05 +0930

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from  "./components/Navbar.js";
+import Header from "./components/Header.js";
+
 
 function App() {
   return (
-    <Navbar/>
+    <>
+    <Particles
+    className = "particles-canvas"
+    params={{
+      particles:{
+        number:{
+          value:20,
+          density:{
+            enable:true, 
+            value_area:1000
+          }
+        },
+        shape:{
+          type:"square",
+          stroke:{
+            width:6,
+            color:"#f9ab00"
+          }
+        }
+      }
+   }
+    }
+    />
+    <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Typed from "react-typed";
+
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+           <div className="main-info">
+              <h1>web development and website promotions</h1>
+              <Typed  
+              className ="typed-text" 
+              strings = {["Web Design", "Web Development", "Facebook Ads SMM", "Google Ads"]}
+              typeSpeed ={70}
+              backSpeed = {70}
+              loop 
+              />
+            <a href="home" className ="btn-main-offer">contact me</a>
+           </div>
+        </div>
+    )
+}
+
+export default Header
